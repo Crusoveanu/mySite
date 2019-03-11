9e9284f7cdf0a13479dd87bb8c876850b7c0e687
unknown <mihai_liviu2009@yahoo.com>
2019-03-11 21:51:40 +0200

    everyting ready for controller

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+        public virtual LibraryCard LIbraryCard { get; set; }
+        public virtual LibraryBranch HomeLIbraryBranch { get; set; }
+    }
+}
